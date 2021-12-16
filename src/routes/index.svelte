11579a55ce6641e364641a0adbe73fffd66b5a5e
Valmisson Grizorte <missongrizorte@gmail.com>
2021-12-16 16:05:48 -0300

    chore: add font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,3 @@
 <h1>Hinário - Uma coleção de canções Cristã</h1>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, quis? Ab unde non molestias explicabo temporibus alias quisquam beatae quos, quia voluptate sed quam nulla inventore numquam corrupti repellat laboriosam.</p>
