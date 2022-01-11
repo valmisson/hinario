198a7f82fc9c84f747a595cbaf06da7aa75224ef
Valmisson Grizorte <missongrizorte@gmail.com>
2022-01-10 21:44:06 -0300

    chore: sort list of hymns

--- src/routes/hinos/index.json.ts
+++ src/routes/hinos/index.json.ts
@@ -16,6 +16,11 @@ export async function get () {
     }
   }))
 
+  // asc order
+  hymns.sort((a, b) => {
+    return a.metadata.number - b.metadata.number
+  })
+
   return {
     body: {
       hymns
