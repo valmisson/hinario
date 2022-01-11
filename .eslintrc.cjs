4983ab3d6e5af578e5f8ae46cbf0bddabb9c9da8
Valmisson Grizorte <missongrizorte@gmail.com>
2022-01-10 21:13:58 -0300

    fix: disable rules that doesn't work on the eslint-plugin-svelte

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,10 @@ module.exports = {
       files: ['*.svelte'],
       processor: 'svelte3/svelte3',
       rules: {
+        'import/first': 0,
+        'import/no-duplicates': 0,
+        'import/no-mutable-exports': 0,
+        'import/no-unresolved': 0,
         'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 2, maxEOF: 0 }]
       }
     }
