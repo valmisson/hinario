08f7dab0cbd2dbb679193cb40ba73be9fc924fe3
Valmisson Grizorte <missongrizorte@gmail.com>
2022-02-28 13:39:23 -0300

    chore: sort hymns

--- src/lib/shared/utils.ts
+++ src/lib/shared/utils.ts
@@ -5,7 +5,9 @@ export function getFiles (path: string): Promise<Array<string>> {
     readdir(path, (err, files) => {
       if (err) return reject(err)
 
-      const filename = files.filter(name => !/^__*/.test(name))
+      const filename = files
+        .filter(name => !/^__*/.test(name))
+        .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
 
       resolve(filename)
     })
